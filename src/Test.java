3e203556891cb4453ac84acaa560a95712005d75
kiran_a <iamkiranashok@gmail.com>
2022-12-19 10:57:35 +0530

    First commit

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Test {
+public class Test {
+    public static void main(String args[])
+    {
+
+        MountainBike mb = new MountainBike(3, 100, 25);
+        System.out.println(mb.toString());
+    }
 }
